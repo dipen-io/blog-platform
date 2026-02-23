d135aa87a2c88a86e18c30c315f331399c8a412e
usernamedinesh <borod9200@gmail.com>
2026-02-23 16:05:10 +0530

    added seeder for category

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'Technology',
+            'Programming',
+            'Busincess',
+            'Health',
+            'Travel',
+            'Education',
+        ];
+
+        foreach ($categories as $category) {
+            Category::create([
+            'name' => $category,
+            'slug' => Str::slug($category)
+        ]);
+        }
+    }
+}
