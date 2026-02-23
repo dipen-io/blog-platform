ca0b84d00f7e8d5fabbd3fe0a5fbcab347a28315
usernamedinesh <borod9200@gmail.com>
2026-02-23 16:34:12 +0530

    fixing model for post and comment

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class Post extends Model
@@ -42,10 +43,13 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
-
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 
     // Accessors
