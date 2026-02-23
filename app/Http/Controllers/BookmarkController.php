d01c10ea8f18326225e1e583f39f7fa0462efd4c
usernamedinesh <borod9200@gmail.com>
2026-02-23 20:17:00 +0530

    added BookmarkController

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bookmark;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class BookmarkController extends Controller
 {
-    //
+    public function toggle(Post $post): RedirectResponse
+    {
+        // make sure user is logged
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        $bookmark = Bookmark::where('post_id', $post->id)
+        ->where('user_id', Auth::id())
+        ->first();
+        if ($bookmark) {
+            $bookmark->delete();
+        } else {
+            Bookmark::create([
+                'post_id' => $post->id,
+                'user_id' => Auth::id(),
+            ]);
+        }
+        return redirect()->back();
+    }
 }
