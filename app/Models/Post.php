b5829cf1bd35acd1cc641c32a33fe79cc915d259
usernamedinesh <borod9200@gmail.com>
2026-02-23 20:17:16 +0530

    fix models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -60,6 +60,10 @@ class Post extends Model
     {
         return $this->likes()->where('user_id', $user->id)->exists();
     }
+    public function bookmarks()
+    {
+        return $this->hasMany(Bookmark::class);
+    }
 
     // Accessors
     public function getThumbnailUrlAttribute(): string
