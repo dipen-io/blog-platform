296f4de968912ebc2146f9c986461ced802d48c9
usernamedinesh <borod9200@gmail.com>
2026-02-23 17:32:09 +0530

    fetch latest-blog from db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Category;
 use App\Models\Post;
 
 class HomeController extends Controller
 {
-    //
     public function index(Request $request)
     {
-        /* $posts = Post::published(); */
-        return view("welcome");
+        // fetch four latest post
+        $latestPosts = Post::with(['user', 'category'])
+            ->where('is_published', true)
+            ->latest()
+            ->take(4)
+            ->get();
+        return view('welcome', compact('latestPosts'));
     }
 }
